9d958b6bd0c5cdae352f1504953115856f90c624
Argenis Lopez <argenislopez2870825633@gmail.com>
2026-05-07 01:38:43 -0300

    fix: update koyeb provider version and remove unsupported type argument

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     koyeb = {
       source  = "koyeb/koyeb"
-      version = "~> 0.1"
+      version = "~> 0.6"
     }
   }
 }
@@ -53,97 +53,81 @@ resource "koyeb_service" "backend" {
     env {
       key   = "AMBIENTE"
       value = var.ambiente
-      type  = "PLAINTEXT"
     }
 
     env {
       key   = "API_HOST"
       value = var.api_host
-      type  = "PLAINTEXT"
     }
 
     env {
       key   = "API_PORT"
       value = var.api_port
-      type  = "PLAINTEXT"
     }
 
     env {
       key   = "DATABASE_URL"
       value = var.database_url
-      type  = "PLAINTEXT"
     }
 
     env {
       key   = "METRICS_BASIC_AUTH_USERNAME"
       value = var.metrics_basic_auth_username
-      type  = "PLAINTEXT"
     }
 
     env {
       key   = "METRICS_BASIC_AUTH_PASSWORD"
       value = var.metrics_basic_auth_password
-      type  = "PLAINTEXT"
     }
 
     env {
       key   = "NOME_APP"
       value = var.nome_app
-      type  = "PLAINTEXT"
     }
 
     env {
       key   = "ORIGENS_PERMITIDAS"
       value = var.origens_permitidas
-      type  = "PLAINTEXT"
     }
 
     env {
       key   = "OTLP_ENDPOINT"
       value = var.otlp_endpoint
-      type  = "PLAINTEXT"
     }
 
     env {
       key   = "REDIS_URL"
       value = var.redis_url
-      type  = "PLAINTEXT"
     }
 
     env {
       key   = "REGEX_ORIGENS_PERMITIDAS"
       value = var.regex_origens_permitidas
-      type  = "PLAINTEXT"
     }
 
     env {
       key   = "RESEND_API_KEY"
       value = var.resend_api_key
-      type  = "PLAINTEXT"
     }
 
     env {
       key   = "RESEND_FROM_EMAIL"
       value = var.resend_from_email
-      type  = "PLAINTEXT"
     }
 
     env {
       key   = "RESEND_TO_EMAIL"
       value = var.resend_to_email
-      type  = "PLAINTEXT"
     }
 
     env {
       key   = "SENTRY_DSN"
       value = var.sentry_dsn
-      type  = "PLAINTEXT"
     }
 
     env {
       key   = "TRUSTED_PROXY_DEPTH"
       value = tostring(var.trusted_proxy_depth)
-      type  = "PLAINTEXT"
     }
 
     health_checks {
